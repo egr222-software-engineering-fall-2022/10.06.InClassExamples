6d5b3faa969cde7327281c4217c2d6c4b47868cf
Larry Clement <lclement@calbaptist.edu>
2022-10-03 15:01:02 -0700

    After initializing section "B" code prior to class

---
+++ Section B/football/src/Main.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DrawingPanel panel = new DrawingPanel(250,250);
+        Graphics g = panel.getGraphics();
+
+
+    }
+}
